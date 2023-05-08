d5c80cc36b3e11398c9701e555f54b867d54b1c9
ibrohimmath <ahmadjonovibrohim062@gmail.com>
2023-05-08 15:57:21 +0500

    project_frontend

--- practice/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from blog.views import *
-
-app_name = 'blog'
-
-urlpatterns = [
-    path('', post_list, name = 'posts'),
-    #path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name = 'post'),
-]
--- practice/blog/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse 
-from django.shortcuts import render
-from blog.models import Post  
-
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts, })
